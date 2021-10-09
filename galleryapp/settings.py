38c786188fffa7fd03df0485c662a2daf98fd7c2
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-10 00:30:03 +0300

    configure image settings

--- galleryapp/settings.py
+++ galleryapp/settings.py
@@ -63,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+
             ],
         },
     },
@@ -125,3 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
