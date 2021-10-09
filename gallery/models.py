c6c097fe53a138bd46fc14447b0fcf55f1479963
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-10 00:25:34 +0300

    add image model

--- gallery/models.py
+++ gallery/models.py
@@ -13,5 +13,15 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
-    
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=100)
+    description=models.TextField()
+    author=models.CharField(max_length=60,default='admin')
+    pub_date=models.DateTimeField(auto_now_add=True)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    location=models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
