7f062a9667089ca936f849065b67339ede856ab3
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-10 00:37:21 +0300

    add category instance test

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    #Set up
+    def setUp(self):
+        self.category=Category(name='Tour')
+        # self.category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
