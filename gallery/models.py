24b1243e720873cfefa683a095b4a312b00935cc
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-10 00:11:08 +0300

    add location model

--- gallery/models.py
+++ gallery/models.py
@@ -4,3 +4,14 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length =60)
 
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name=models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    
+
