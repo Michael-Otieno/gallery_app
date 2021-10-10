172cb3dddb3a004dfc079ad3ee5a5d3b2d186f5c
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-10 03:48:20 +0300

    add location function to views

--- gallery/views.py
+++ gallery/views.py
@@ -7,3 +7,9 @@ def main(request):
     locations=Location.get_locations()
     print(locations)
     return render(request,'gallery/index.html',{'images:':images[::-1],'locations':locations})
+
+def location(request, location):
+    images= Image.filter_by_location(location)
+    print(images)
+    return render(request,'gallery/location.html',{'location_images':images})
+
