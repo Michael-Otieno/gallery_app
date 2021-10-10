f52382dda05a9735dbd7c41776dd03d6cbc6f309
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-10 03:42:30 +0300

    add main view function and url

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns=[
-    url('^$',views.index, name='index'),
+    url('^$',views.main, name='main'),
 ]
 
 if settings.DEBUG:
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Image,Location
 # Create your views here.
-def index(request):
-    return HttpResponse('Index page')
+def main(request):
+    images=Image.objects.all()
+    locations=Location.get_locations()
+    print(locations)
+    return render(request,'gallery/index.html',{'images:':images[::-1],'locations':locations})
