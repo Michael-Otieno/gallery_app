b6db1e6807062b282c8c40a3fa8f728620aaa193
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-10 09:34:27 +0300

    add bootstrap5

--- gallery/views.py
+++ gallery/views.py
@@ -6,12 +6,12 @@ def main(request):
     images=Image.objects.all()
     locations=Location.get_locations()
     print(locations)
-    return render(request,'gallery/index.html',{'images:':images[::-1],'locations':locations})
+    return render(request,'galleries/main.html',{'images:':images[::-1],'locations':locations})
 
 def location(request, location):
     images= Image.filter_by_location(location)
     print(images)
-    return render(request,'gallery/location.html',{'location_images':images})
+    return render(request,'galleries/location.html',{'location_images':images})
 
 def search(request):
     if 'searchedimage' in request.GET and request.GET['searchedimage']:
@@ -19,8 +19,8 @@ def search(request):
         searched_images=Image.search_by_category(category)
         message=f'{category}'
         print(searched_images)
-        return render(request, 'gallery/search.html',{'message':message,'images':searched_images})
+        return render(request, 'galleries/search.html',{'message':message,'images':searched_images})
     
     else:
         message='You have not searched for any image category'
-        return render(request,'gallery/search.html',{'message':message})
+        return render(request,'galleries/search.html',{'message':message})
